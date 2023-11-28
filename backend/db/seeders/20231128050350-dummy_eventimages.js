ce153d77f375315acdb95dce35d2727ef541b01f
Brandon <b.n.tamayo7@gmail.com>
2023-11-28 08:27:57 -0800

    Added back in event images, still no Attendance

---
+++ backend/db/seeders/20231128050350-dummy_eventimages.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+const { EventImage } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await EventImage.bulkCreate([
+    {
+      eventId: 1,
+      url: 'https://example.com/',
+      preview: false
+    },
+    {
+      eventId: 2,
+      url: 'https://i.imgur.com/CHE6ilh.jpg',
+      preview: true
+    },
+    {
+      eventId: 3,
+      url: 'https://vercel.com/new/templates',
+      preview: false
+    }
+   ])
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'EventImages';
+
+    const Op = Sequelize.Op;
+
+    return await queryInterface.bulkDelete(options, {
+      url: {
+        [Op.in]: ['https://vercel.com/new/templates', 'https://i.imgur.com/CHE6ilh.jpg', 'https://example.com/']
+      }
+    }, {})
+  }
+};
