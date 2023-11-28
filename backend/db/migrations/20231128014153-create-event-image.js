a54b561ad007fd2102ec375176c380a064c5e2ac
Brandon <b.n.tamayo7@gmail.com>
2023-11-27 21:55:04 -0800

    Fixed options bug

--- backend/db/migrations/20231128014153-create-event-image.js
+++ backend/db/migrations/20231128014153-create-event-image.js
@@ -1,5 +1,13 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+const { EventImage } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('EventImages', {
@@ -38,6 +46,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('EventImages');
+    options.tableName = 'EventImages'
+    await queryInterface.dropTable(options);
   }
 };
