abd187bdd6dd91ac1496c080f84f6799aa6c3836
Brandon <b.n.tamayo7@gmail.com>
2023-11-27 21:11:03 -0800

    Added eventimage seed file

--- backend/db/seeders/20231128050350-dummy_eventimages.js
+++ backend/db/seeders/20231128050350-dummy_eventimages.js
@@ -1,6 +1,14 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+const { EventImage } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -12,6 +20,24 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+   await EventImage.bulkCreate([
+    {
+      eventId: 1,
+      url: 'https://example.com/',
+      preview: false
+    },
+    {
+      eventId: 2,
+      url: 'https://i.imgur.com/CHE6ilh.jpg',
+      preview: true
+    },
+    {
+      eventId: 3,
+      url: 'https://vercel.com/new/templates',
+      preview: false
+    }
+   ])
+
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +47,14 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    options.tableName = 'EventImages';
+
+    const Op = Sequelize.Op;
+
+    return await queryInterface.bulkDelete(options, {
+      url: {
+        [Op.in]: ['https://vercel.com/new/templates', 'https://i.imgur.com/CHE6ilh.jpg', 'https://example.com/']
+      }
+    }, {})
   }
 };
