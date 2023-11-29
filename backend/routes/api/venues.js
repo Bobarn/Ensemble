e3388d865bb075457f79bfa7e6ca838a2703c839
Brandon <b.n.tamayo7@gmail.com>
2023-11-28 21:35:27 -0800

    Finished venue endpoints and added venue authorization

--- backend/routes/api/venues.js
+++ backend/routes/api/venues.js
@@ -3,14 +3,72 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth, authorize } = require('../../utils/auth');
-const { Group, Membership, Venue } = require('../../db/models');
+const { Venue } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateVenueEdit = [
+    check('address')
+      .optional()
+      .isString()
+      .withMessage('Street address is required'),
+    check('city')
+       .optional()
+       .isString()
+       .isAlpha('en-US', {ignore: [' ', '-']})
+       .withMessage('City is required'),
+    check('state')
+      .optional()
+      .isString()
+      .isAlpha('en-US', {ignore: [' ', '-']})
+      .withMessage('State is required'),
+    check('lat')
+      .optional()
+      .isFloat({min: -90, max: 90})
+      .withMessage('Latitude is not valid'),
+    check('lng')
+      .optional()
+      .isFloat({min: -180, max: 180})
+      .withMessage('Longitude is not valid'),
+    handleValidationErrors
+  ];
+
+router.put('/:venueId',requireAuth, authorize, validateVenueEdit, async (req, res) => {
 
 
-router.get
+
+    const id = parseInt(req.params.venueId);
+
+    const { address, city, state, lat, lng } = req.body;
+
+    const venue = await Venue.findByPk(id);
+
+    if(!venue) {
+
+        res.status(404);
+       return res.json({
+            message: "Venue couldn't be found"
+        })
+    }
+
+    venue.address = address || venue.address;
+
+    venue.city = city || venue.city;
+
+    venue.state = state || venue.state;
+
+    venue.lat = lat !== undefined ? lat : venue.lat;
+
+    venue.lng = lng !== undefined ? lng : venue.lng;
+
+    venue.save();
+
+    const result = await Venue.findByPk(id);
+
+    res.json(result)
+
+})
 
 module.exports = router;
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,7 +1,7 @@
 // backend/utils/auth.js
 const jwt = require('jsonwebtoken');
 const { jwtConfig } = require('../config');
-const { User, Group, Membership } = require('../db/models');
+const { User, Group, Membership, Venue } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
 
@@ -78,30 +78,64 @@ const requireAuth = function (req, _res, next) {
 const authorize = async function (req, res, next) {
   const { user } = req;
 
+  let group;
+
+  let venue;
+
+  let status;
+
+  let venueGroup;
+
   const groupId = parseInt(req.params.groupId);
 
-  const group = await Group.findByPk(groupId);
+  const venueId = parseInt(req.params.venueId);
 
-  const status = await Membership.findOne({
-    where: {
-      userId: user.id,
-      groupId: groupId
-    }
-  })
+  if(groupId) {
+
+    group = await Group.findByPk(groupId);
+
+    status = await Membership.findOne({
+       where: {
+         userId: user.id,
+         groupId: groupId
+       }
+     })
+  } else {
+     venue = await Venue.findByPk(venueId);
+
+     venueGroup = await Group.findByPk(venue.groupId);
+  }
+
+
+
 
   // console.log("===============", status.status);
 
   // console.log(user);
+if(group) {
 
-  if(user.id == group.organizerId || status.status === 'co-host') {
+    if(user.id == group.organizerId || status.status === 'co-host') {
+      return next();
+    }
+    else {
+      const err = new Error('Forbidden');
+      err.title = 'Require proper authorization'
+      err.status = 403;
+      err.errors = { message: 'Require proper authorization'};
+      return next(err);
+    }
+} else if(venueGroup) {
+  if (venueGroup.organizerId === user.id) {
     return next();
-  } else {
+  }
+  else {
     const err = new Error('Forbidden');
     err.title = 'Require proper authorization'
     err.status = 403;
     err.errors = { message: 'Require proper authorization'};
     return next(err);
   }
+}
 }
 
 const checkId = async function (req, res, next) {
